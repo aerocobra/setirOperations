854eeff6d2e44f3e54e27e988bd488af51a55d98
aerocobra <zmeyg@yahoo.com>
2017-09-25 17:23:03 +0200

    project creation in git

---
+++ __openerp__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# © 2017 Igor V. Kartashov
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": 'SETIR Operatopn',
+    "version": "9.0.1.0",
+    "summary": "módulo ventas para SETIR",
+    "description": """
+            		i-vk
+            		v9.0.1.0
+            		
+            		Operaciones para SETIR
+                	""",
+    "author": "Igor V. Kartashov",
+    "license": "AGPL-3",
+    "website": "http://crm.setir.es",
+    "category": "Sale",
+    "depends": [
+				'base',
+				'crm',
+				'sale',
+				'website_quote',
+                'mail',
+				],
+    "data": [
+		#"views/setirSaleOrderForm.xml",
+        ],
+    "installable": True,
+    "application": True,
+	"auto_install": False,
+    "price": 0.00,
+    "currency": "EUR"
+}
