fcdaf049d8df6a72a7453e5aef59a21c26140e32
aerocobra <zmeyg@yahoo.com>
2017-09-26 15:39:46 +0200

    add

--- models/setirOperation.py
+++ models/setirOperation.py
@@ -11,6 +11,35 @@ from openerp.tools import float_is_zero, float_compare, DEFAULT_SERVER_DATETIME_
 from openerp import exceptions
 
 class setirOperation ( models.Model):
-    _name = "setir.operation"
+	_name	= "setir.operation"
 
-    x_strName   = fields.Char ( string = "Operaci�n")
+	name	= fields.Char ( string = "Operación")
+
+class setirPMState ( models.Model):
+	_name = "setir.pm.state"
+
+	name            = fields.Char ( string = "Estado")
+	strDescription  = fields.Char ( string = "Descripción")
+
+class setirPMUnsubscribeReason ( models.Model):
+	_name = "setir.pm.unsubscribe.reason"
+
+	name            = fields.Char ( string = "Razón")
+
+class setirPMBlockReason ( models.Model):
+	_name = "setir.pm.block.reason"
+
+	name            = fields.Char ( string = "Razón")
+
+class setirPMStateHistory ( models.Model):
+	_name = "setir.pm.state.history"
+	
+	idState	= fields.Many2one ( string			= "Estado",
+								comodel_name	= "setir.pm.state")
+	dtState	= fields.DateTime (	string	= "Fecha")	
+
+class setirPMManagement ( models.Model):
+	_name = "setir.pm.management"
+
+	name    = fields.Char ( string = "Gestión")
+	
