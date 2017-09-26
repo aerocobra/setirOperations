82e9d96a237fd75029be769ff1eef98853494cff
aerocobra <zmeyg@yahoo.com>
2017-09-26 09:55:33 +0200

    two

---
+++ models/setirOperation.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#setirSale.py
+import openerp.addons.decimal_precision as dp
+from openerp import api, fields, models
+from openerp import tools
+from pygments.lexer import _inherit
+from datetime import datetime, timedelta
+from openerp import SUPERUSER_ID
+from openerp.exceptions import UserError
+from openerp.tools import float_is_zero, float_compare, DEFAULT_SERVER_DATETIME_FORMAT
+from openerp import exceptions
+
+class setirOperation ( models.Model):
+    _name = "setir.operation"
+
+    x_strName   = fields.Char ( string = "Operación")
